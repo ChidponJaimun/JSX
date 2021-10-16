a2629fabdde88db32372ecbb79b30824b479755f
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-17 01:17:15 +0700

    change import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const React = require("react");
-const ReactDom = require("react-dom");
+import React from "react";
+import ReactDom from "react-dom";
 
 ReactDom.render(<h1>Hello World!</h1>, document.getElementById("root"));
