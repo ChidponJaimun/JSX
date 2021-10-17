421d60d988def271af23417da37f0d3a339021e8
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-17 17:22:35 +0700

    2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-ReactDom.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDom.render(
+  <div>
+    <h1>My Favourite Game.</h1>
+    <ul>
+      <li>Apex Legend</li>
+      <li>LOL TFT</li>
+      <li>PUBG</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
