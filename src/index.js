bb6d4c4cd2cde5bcbdf12348e21b0865134c6779
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-17 19:15:49 +0700

    injection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const name = "Joe";
+const fName = "Chidpon";
+const lName = "Jaimun";
 const luckyNumber = 13;
 ReactDom.render(
   <div>
-    <h1>Hello! {name}.</h1>
+    <h1>
+      Hello! {fName} {lName} .
+    </h1>
     <p>Your lucky number is {luckyNumber}.</p>
   </div>,
   document.getElementById("root")
